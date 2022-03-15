1e50a5eaa38004d422054161fd33f7a508f0629d
Frcomparan <frcomparan@gmail.com>
2022-03-14 18:32:14 -0600

    Generate structures and files for log_in view
    Add a temporary routes to see the changes in the view
    Add a new view in admins to make the design
    Add the basic HTML structure for view

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,5 @@
 class AdminsController < ApplicationController
+  layout "login", only: [:log_in]
   before_action :set_admin, only: %i[ show edit update destroy ]
 
   # GET /admins or /admins.json
@@ -66,5 +67,8 @@ class AdminsController < ApplicationController
     # Only allow a list of trusted parameters through.
     def admin_params
       params.require(:admin).permit(:name, :password)
+    end
+
+    def log_in
     end
 end

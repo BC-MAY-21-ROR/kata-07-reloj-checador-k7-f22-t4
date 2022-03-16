14075f0810259861870d1f8695d4b70fc11ca159
Frcomparan <frcomparan@gmail.com>
2022-03-16 15:16:17 -0600

    Re-built Admin model to implement a basic auth
    Elimianted Admin model generated with scaffold
    Added Admin model generate with devise
    Created a migration to add name attribute to Admin model
    
    Co-authored-by: brocha05 <brocha_05@hotmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins
   resources :attendances
   resources :employees
-  resources :admins
   resources :branches
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   

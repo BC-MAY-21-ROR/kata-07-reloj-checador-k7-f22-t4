b199303c59f36080ea34a07f1c20bde46d9516de
Frcomparan <frcomparan@gmail.com>
2022-03-20 12:34:51 -0600

    Configure styles for "sing_in / new" view
    Co-authored-by: brocha05 <brocha_05@hotmail.com>

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,5 +2,4 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link sass/views/admins/log_in.css
-//= link_tree ../stylesheets/sass/views/ .css
+//= link sass/main.css

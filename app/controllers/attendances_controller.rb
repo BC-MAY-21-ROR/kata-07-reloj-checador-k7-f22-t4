30fde4153bdc9a6cb26aa92a16707adf1a781d7c
Frcomparan <frcomparan@gmail.com>
2022-03-23 17:02:46 -0600

    Add functionality to save complete record
    It should not be possible to make a new record if the last one is not complete

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,6 @@
 class AttendancesController < ApplicationController
   before_action :set_attendance, only: %i[show edit update destroy]
-  before_action :authenticate_admin!, except: [:new]
+  before_action :authenticate_admin!, except: [:new, :create]
 
   # GET /attendances or /attendances.json
   def index
@@ -16,56 +16,49 @@ class AttendancesController < ApplicationController
     @attendance = Attendance.new
   end
 
-  # GET /attendances/1/edit
-  def edit
-  end
+  def check; end
 
   # POST /attendances or /attendances.json
   def create
-    @attendance = Attendance.new(attendance_params)
-
-    respond_to do |format|
-      if @attendance.save
-        format.html { redirect_to attendance_url(@attendance), notice: "Attendance was successfully created." }
-        format.json { render :show, status: :created, location: @attendance }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @attendance.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /attendances/1 or /attendances/1.json
-  def update
-    respond_to do |format|
-      if @attendance.update(attendance_params)
-        format.html { redirect_to attendance_url(@attendance), notice: "Attendance was successfully updated." }
-        format.json { render :show, status: :ok, location: @attendance }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @attendance.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /attendances/1 or /attendances/1.json
-  def destroy
-    @attendance.destroy
-
-    respond_to do |format|
-      format.html { redirect_to attendances_url, notice: "Attendance was successfully destroyed." }
-      format.json { head :no_content }
+    @employee = Employee.find_by(private_code: attendance_params[:employee_id])
+    if @employee && !helpers.check_complete? then new_check_in
+    elsif @employee && helpers.check_complete? then new_check_out
+    else redirect_to new_attendance_path, alert: "Employee doesn't exist"
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_attendance
-      @attendance = Attendance.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def attendance_params
-      params.require(:attendance).permit(:check_in, :check_out, :employee_id)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_attendance
+    @attendance = Attendance.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def attendance_params
+    params.require(:attendance).permit(:employee_id)
+  end
+
+  def new_check_in
+    new_atte = { check_in: DateTime.now, employee_id: @employee.id }
+    @attendance = Attendance.new(new_atte)
+    respond_to do |format|
+      if @attendance.save then 
+       format.html { redirect_to new_attendance_path, notice: 'CHECK IIIINNN' }
+      else
+       format.html { render :new, status: :unprocessable_entity }
+      end
     end
+  end
+
+  def new_check_out
+    @attendance = @employee.attendances.last
+    respond_to do |format|
+      if @attendance.update(check_out: DateTime.now)
+        format.html { redirect_to new_attendance_path, notice: 'CHECK OOOUUUTT' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
 end

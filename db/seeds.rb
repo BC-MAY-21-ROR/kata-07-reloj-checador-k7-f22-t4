1cd76d68ab90d8576fcbb924240d882ebe9dae82
Frcomparan <frcomparan@gmail.com>
2022-03-24 16:11:56 -0600

    Add some default data for the DB to tests
    Co-authored-by: DiegoSteve <diegosteve123321@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Branch.create(name: 'Human Resources', address: 'Av. Tecnologico')
+Branch.create(name: 'Finances', address: 'Tercer Anillo Periferico')
+Branch.create(name: 'Systems', address: 'Nowhere')
+
+Employee.create(name: 'Marco Polo', email: 'example@gmail.com', private_code: '325353', branch_id: 1)
+Employee.create(name: 'Aurelio Gómez', email: 'example@outlook.com', private_code: '908731', branch_id: 1)
+Employee.create(name: 'Samuel De Luque', email: 'example@hotmail.com', private_code: '312315', branch_id: 1)
+Employee.create(name: 'Guillermo Diaz', email: 'example@live.com', private_code: '876543', branch_id: 1)
+
+13.times do |month| 
+  4.times { | employee | Attendance.create(check_in: (Date.today - month.months), check_out: Date.today + 2.hours - month.months, employee_id: employee + 1) }
+end

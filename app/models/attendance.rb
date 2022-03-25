9dc13c4eaa78ac3ee959c17fd0646a888036b5e2
Frcomparan <frcomparan@gmail.com>
2022-03-24 18:50:00 -0600

    Add method to get absence report by month
    Co-authored-by: DiegoSteve <diegosteve123321@gmail.com>

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -26,4 +26,14 @@ class Attendance < ApplicationRecord
     attendances_list.each { |date| check_in_average += date.hour }
     check_in_average /= attendances_list.length
   end
+
+  def absence_by_month(month, employee_id)
+    employee = Employee.find_by(id: employee_id)
+    attendances = employee.attendances.where(check_in: Date.parse(month)..Date.parse(month) + 1.months)
+    attendances.legth - efective_day_of_month(month).length
+  end
+
+  def efective_day_of_month(month)
+    (Date.parse(month)...Date.parse(month) + 1.months).reject { |day| day.wday == 6 || day.wday.zero? }
+  end
 end

1a5a1d458c9a5d31f99f89c0d69619c092be6847
Frcomparan <frcomparan@gmail.com>
2022-03-24 18:48:07 -0600

    Add method to get attendance by day
    Co-authored-by: DiegoSteve <diegosteve123321@gmail.com>

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,7 @@
 class Attendance < ApplicationRecord
   belongs_to :employee
+
+  def attendance_by_day(day)
+    Attendance.all.where(check_in: Date.parse(day).beginning_of_day..Date.parse(day).end_of_day)
+  end
 end

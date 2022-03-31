e96b8e2fbb1c057f6575b3b809c84a1c0a311983
heikel <heikel@ecc.cu>
2022-03-31 17:15:44 -0600

    solved CR recommendations

--- app/javascript/employees.js
+++ app/javascript/employees.js
@@ -1,17 +1,17 @@
- setActives = (employee) => {
-     var csrf = document.querySelector('meta[name="csrf-token"]').content;
-     var xhr = new XMLHttpRequest();
-     xhr.open("DELETE", "employees/"+ employee);
-     xhr.setRequestHeader('X-CSRF-Token', csrf);
-     xhr.send();
-     xhr.onload = function() {
-         if (xhr.status != 200) {
-             console.log('ERROR');
-         } else {
-             console.log('DELETED!');
-         }
-     };
-     xhr.onerror = function() {
-         console.log('NO CONNECTION');
-     };
-}
+setActives = (employee) => {
+  var csrf = document.querySelector('meta[name="csrf-token"]').content;
+  var xhr = new XMLHttpRequest();
+  xhr.open("DELETE", "employees/" + employee);
+  xhr.setRequestHeader("X-CSRF-Token", csrf);
+  xhr.send();
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+      alert("Ocurrió un error");
+    } else {
+        alert("Operación exitosa");
+    }
+  };
+  xhr.onerror = function () {
+    console.log("Error de conexión");
+  };
+};
